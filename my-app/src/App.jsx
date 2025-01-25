b3ff54fb52d73b62f6fb0e51510a6ef9e25f1055
Hardik Marlapudi <hardik.a.marlapudi@gmail.com>
2025-01-25 00:21:43 -0500

    adding new version of react-practice

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './main.jsx'
+
+function App() {
+  return (
+    <div className="title">
+      <h1>Hardik Marlapudi</h1>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ my-app/src/TechnicalExperience.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './main.jsx'
+import './Teaching&Technical.css'
+
+function TechnicalExperience() {
+    return (
+        <div id = "teaching-technical">
+            <h2>Teaching & Technical</h2>
+        </div>
+
+        
+    )
+
+    
+}
+
+export default TechnicalExperience;
